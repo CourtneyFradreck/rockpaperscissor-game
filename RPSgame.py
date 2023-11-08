d970df29f2539bdf4cc9294045e640c2e36dc9dc
CourtneyFradreck <courtaga@gmail.com>
2023-11-08 19:22:00 +0200

    first commit

---
+++ RPSgame.py
@@ -0,0 +1,42 @@
+import random
+
+print("Hi, Welcome!! "
+      "Lets Play a Game!!!")
+Game = ["Rock", "Paper", "Scissors"]
+player = input("Choose Between Rock, Paper or Scissors: ")
+print(f"You Chose {player}")
+
+computer = random.choice(Game)
+print(f"Computer Chose {computer}")
+
+if computer == "Rock" and player == "Rock":
+    print("Congratulations, Its a Draw!! We Both Chose Rock")
+
+elif computer == "Rock" and player == "Paper":
+    print("Congratulations!! You Won!!")
+
+elif computer == "Rock" and player == "Scissors":
+    print("Got You!! Hah Hah Hah!! I won")
+
+elif computer == "Paper" and player == "Rock":
+    print("Aww, Sorry! I won")
+
+elif computer == "Paper" and player == "Paper":
+    print("Wow!! It's a Draw")
+
+elif computer == "Paper" and player == "Scissors":
+    print("Nice Try!! You Won!! Wanna Go Again?")
+
+elif computer == "Scissors" and player == "Rock":
+    print("Congrats!! You beat me!")
+
+elif computer == "Scissors" and player == "Paper":
+    print("Hah Hah Hah!!! You're a Loser")
+
+elif computer == "Scissors" and player == "Scissors":
+    print("Oh No Its A Draw!!")
+
+else:
+    print("You Chose an Invalid Option...I Won!!")
+
+# Put a Funtion To Keep High Scores
